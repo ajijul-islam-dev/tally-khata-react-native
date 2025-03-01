b1fad1a67deaaefe5aa3a960894151d7c4c8ae43
ajijul-islam-dev <ajijulislamdev@gmail.com>
2025-03-02 01:11:40 +0600

    basic navigation created

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import {Text} from 'react-native';
 import {Button} from '@tamagui/button';
 import HomeScreen from './screens/HomeScreen';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
 
 // you usually export this from a tamagui.config.ts file
 const config = createTamagui(defaultConfig);
@@ -16,10 +19,24 @@ declare module '@tamagui/core' {
 }
 
 export default () => {
+  const Stack = createNativeStackNavigator();
   return (
     <TamaguiProvider config={config}>
       <SafeAreaProvider>
-        <HomeScreen />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{headerShown: true}}
+            />
+            <Stack.Screen
+              name="LoginScreen"
+              component={LoginScreen}
+              options={{headerShown: true, title: 'Login'}}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
       </SafeAreaProvider>
     </TamaguiProvider>
   );
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 import {Button} from '@tamagui/button';
 import {useNavigation} from '@react-navigation/native';
 
-const HomeScreen = () => {
+const LoginScreen = () => {
   const navigation = useNavigation();
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="bg-red-500 text-white">HomeScreen</Text>
-      <Button onPress={() => navigation.navigate('LoginScreen')}>Login</Button>
+    <View className="flex-1 justify-center items-center">
+      <Text>LoginScreen</Text>
+      <Button onPress={() => navigation.goBack()}>Go Home</Button>
     </View>
   );
 };
 
-export default HomeScreen;
+export default LoginScreen;
 
 const styles = StyleSheet.create({});
